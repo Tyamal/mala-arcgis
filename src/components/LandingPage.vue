a1355dc5a538af2f5a6307a10677c3039efebed2
Mal <anityakumala@gmail.com>
2025-02-26 05:36:08 +0700

    Create LandingPage.vue

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="landing-page">
+    <h1>Welcome to Mala ArcGIS</h1>
+    <p>Your one-stop solution for geographic data visualization.</p>
+    <router-link to="/map">Go to Map</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+};
+</script>
+
+<style scoped>
+.landing-page {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
